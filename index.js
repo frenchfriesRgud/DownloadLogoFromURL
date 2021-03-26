3d5be4213afe512b51cf3fdc4ac27a0885ce30e0
anthony <48930658+frenchfriesRgud@users.noreply.github.com>
2021-03-26 17:25:58 -0400

    working bulk logo downloader

--- index.js
+++ index.js
@@ -1,26 +1,66 @@
 const fs = require('fs')
 const request = require('request');
-const SYMBOL = [
-    'VZ'
-]
+var http = require('http');
+const SYMBOL = require('./data/SYMBOL.json');
 
+const NOTFOUND = [];
 
 // IMAGE DOWNLOAD FUNCTION
-const download = function(uri, sym, filename, callback){
-    request.head(uri, function(err, res, body){
-        if(res.headers['content-type'] != 'image/png'){
-            return console.log(`${sym} - not found`)
-        }
+// const download = function(uri, sym, filename, callback){
+//     request.head(uri, function(err, res, body){
+//         if(res.headers['content-type'] != 'image/png'){
+//             NOTFOUND.push(sym);
+//             return console.log(`${sym} - not found`)
+//         }
 
-        console.log(`${sym}.png - ${(res.headers['content-length'] / 1024).toFixed(2)} Kb`, );
+//         console.log(`${sym}.png - ${(res.headers['content-length'] / 1024).toFixed(2)} Kb`, );
 
-        request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-    });
-};
+//         request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+//     });
+// };
 
+// const start = () => {
+//     for(let sym of SYMBOL){
+//         download(`https://g.foolcdn.com/art/companylogos/square/${sym.symbol.toLowerCase()}.png`, sym.symbol, `download/foolcdn/${sym.symbol}.jpg`, function(){
+//             console.log(`${sym.symbol}.jpg - saved to disk.`);
+//         });
+//     }
+// }
 
-for(let sym of SYMBOL){
-    download(`https://g.foolcdn.com/art/companylogos/square/${sym.toLowerCase()}.png`, sym, `download/foolcdn/${sym}.jpg`, function(){
-        console.log(`${sym}.jpg - saved to disk.`);
-    });
+var rDownload = function (urlArray, i) {
+    if (i < urlArray.length) {
+        request.get(`https://g.foolcdn.com/art/companylogos/mark/${urlArray[i].symbol.toLowerCase()}.png`)                                                                                  
+            .on('error', function(err) {
+                console.log(`${urlArray[i].symbol} - not found`);
+                //rDownload(urlArray, i+1);
+            })
+            .on('response', function(response) {
+                if(response.statusCode != 200){
+                    //throw new Error('not found');
+                    console.log(`${urlArray[i].symbol} - not found`)
+                    //return rDownload(urlArray, i+1);
+                }
+            })                                                   
+            .pipe(fs.createWriteStream(`download/foolcdn_mark/${urlArray[i].symbol}.jpg`))                                                                 
+            .on('close', function () { 
+                console.log(`${urlArray[i].symbol} - saved`);
+                rDownload(urlArray, i+1); })
+            .on('error', function(err){
+                console.log('error thrown')
+            })
+    }
 }
+
+// let SYMBOL = [
+//     {
+//     symbol: 'AAPL'
+//     },
+//     {
+//         symbol: 'alklkdsfklsd'
+//     },
+//     {
+//     symbol: 'AMD'
+//     },
+// ]
+
+rDownload(SYMBOL, 0);
